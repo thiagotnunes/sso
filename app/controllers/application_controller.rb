de1d5cce01bd93c7336e800de6fbc1630f46adba
Thiago Nunes <thiagotnunes@gmail.com>
2012-02-01 19:17:03 -0200

    Adding before filter to check for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  protected
+
+  def authenticate!
+    redirect_to sign_in_url unless session[:user_id] 
+  end
+
   private
 
   def current_user
--- app/controllers/secure_controller.rb
+++ app/controllers/secure_controller.rb
@@ -1,4 +1,6 @@
 class SecureController < ApplicationController
+  before_filter :authenticate!
+
   def index
   end
 end
